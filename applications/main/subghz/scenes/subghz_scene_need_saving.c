b041177398b88c53991a72293ba6a7791c9ebd31
grugnoymeme <47lecoste@tuta.io>
2026-03-17 22:18:26 +0100

    fixing problem of exit confirm not working when entering subghz app with right arrow ALREADY WIP

--- applications/main/subghz/scenes/subghz_scene_need_saving.c
+++ applications/main/subghz/scenes/subghz_scene_need_saving.c
@@ -45,25 +45,24 @@ bool subghz_scene_need_saving_on_event(void* context, SceneManagerEvent event) {
             subghz_rx_key_state_set(subghz, SubGhzRxKeyStateBack);
             scene_manager_previous_scene(subghz->scene_manager);
             return true;
-        } else if(event.event == SubGhzCustomEventSceneExit) {
+	} else if(event.event == SubGhzCustomEventSceneExit) {
             SubGhzRxKeyState state = subghz_rx_key_state_get(subghz);
             subghz_rx_key_state_set(subghz, SubGhzRxKeyStateIDLE);
-
             if(state == SubGhzRxKeyStateExit) {
                 if(scene_manager_has_previous_scene(subghz->scene_manager, SubGhzSceneReadRAW)) {
                     if(!furi_string_empty(subghz->file_path_tmp)) {
                         subghz_delete_file(subghz);
                     }
                 }
-
                 subghz_txrx_set_preset(
                     subghz->txrx, "AM650", subghz->last_settings->frequency, NULL, 0);
-                scene_manager_search_and_switch_to_previous_scene(
-                    subghz->scene_manager, SubGhzSceneStart);
+                if(!scene_manager_search_and_switch_to_previous_scene(
+                       subghz->scene_manager, SubGhzSceneStart)) {
+                    scene_manager_previous_scene(subghz->scene_manager);
+                }
             } else {
                 scene_manager_previous_scene(subghz->scene_manager);
             }
-
             return true;
         }
     }
